e1a32e51a2a0c6cc2ed7ebf201b14174736d19f8
mriolfo <matuteluciano@hotmail.com>
2019-11-24 13:58:31 -0500

    adding function def

---
+++ code/helloWorld.py
@@ -0,0 +1,10 @@
+# print("Hello World")
+
+class HelloWorld():
+
+    @staticmethod
+    def def_hello_world(hello_world):
+        hello_world = "Hello World"
+        # typeYourName = input("What is your name? ")
+        # print("{}".format(hello_world), " {}".format(typeYourName))
+        return hello_world
