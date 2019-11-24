839db5833007ddbc0a0ba56dd90e4d947e38a305
mriolfo <matuteluciano@hotmail.com>
2019-11-24 14:39:31 -0500

    adding class to maxNumber

--- test/maxNumberTest.py
+++ test/maxNumberTest.py
@@ -7,7 +7,7 @@ class MaxNumberTest(unittest.TestCase):
     # function test name always must start with test_function_to_test
 
     def test_def_max_number(self):
-        self.assertEqual()
+        pass
 
 
 if __name__ == '__main__':
