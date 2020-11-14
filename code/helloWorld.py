3e389d00dd81d9940f07b7ef2d99057df9155b07
mriolfo <matuteluciano@hotmail.com>
2020-11-14 11:13:28 -0500

    Adding !

--- code/helloWorld.py
+++ code/helloWorld.py
@@ -4,7 +4,7 @@ class HelloWorld():
 
     @staticmethod
     def def_hello_world(hello_world):
-        hello_world = "Hello World"
+        hello_world = "Hello World !"
         # typeYourName = input("What is your name? ")
         # print("{}".format(hello_world), " {}".format(typeYourName))
         return hello_world
